9d9ea0fb8695096dc9b41a10ef899adf8f158b04
Timur Saglam <uleay@student.kit.edu>
2017-07-19 11:59:33 +0200

    ADD: Added test classes for a simple case.

---
+++ src/jce/simple/BusinessPerson.java
@@ -0,0 +1,10 @@
+package jce.simple;
+
+import java.util.Date;
+
+/**
+ * This is a interface for testing purposes of the Java code ecorification.
+ */
+public interface BusinessPerson {
+    public void generateReport(Date day);
+}
---
+++ src/jce/simple/Person.java
@@ -0,0 +1,29 @@
+package jce.simple;
+
+/**
+ * This is a class for testing purposes of the Java code ecorification.
+ */
+public class Person {
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
