a9fdb5ba9de39b271da11f75c2ee7fcf03e72f78
Timur Saglam <uleay@student.kit.edu>
2017-12-10 12:14:01 +0100

    ADD: Added three basic classes for testing the wrapping.

---
+++ src/jce/wrapping/MidLevelClass.java
@@ -0,0 +1,11 @@
+package jce.wrapping;
+
+/**
+ * This is a class for testing purposes of the Java code ecorification.
+ */
+public abstract class MidLevelClass extends TopLevelClass {
+
+    public MidLevelClass() {
+        System.out.println("MidLevelClass Constructor");
+    }
+}
